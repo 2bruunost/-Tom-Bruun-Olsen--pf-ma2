23c0f319f79bd4ed0746f71edc5a3c522fe5e4cb
2bruunost <tomrbuunolsen@gmail.com>
2020-10-11 16:56:50 +0200

    Commit MA2

---
+++ script.js
@@ -0,0 +1,42 @@
+// question 1
+var petArray = ["cat", "cow", "dog"];
+
+// question 2
+console.log(petArray[1]);
+
+// question 3
+petArray.push("sheep");
+
+// question 4
+console.log(petArray.length);
+
+// question 5
+var catObject = {
+	name: "Felix",
+	age: 2,
+	colour: "red",
+};
+
+// question 6
+console.log(catObject.colour);
+
+// question 7
+var catArray = [
+	{
+		name: "Felix",
+		age: 2,
+		colour: "red",
+	},
+];
+
+// question 8
+for (var i = 0; i < catArray.length; i++) {
+	console.log(catArray[i]);
+}
+
+// question 9
+function logToConsole(wordLog) {
+	console.log(wordLog);
+}
+// question 10
+logToConsole("Donkey");
